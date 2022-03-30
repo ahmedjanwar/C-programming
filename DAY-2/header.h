20d4531ccaee94ec704f273955f39b453f018617
e2001332 <e2001332@WA2029-17>
2022-03-30 13:51:04 +0300

    Add project files.

---
+++ DAY-2/header.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define MAX 50
+enum COLORS {RED = 31, GREEN, YELLOW, BLUE, MAGENTA, CYAN};
+
+void sort_score(int arr[], int n);
+void unsort_score(int arr[], int n);
+void print_sorted(int arr[], int n);
+double avg_score(int arr[], int n);
+
+void resetColors(void);
+void setForeground(int color);
+void setBackground(int color);
+void clearScreen(void);
+void gotoXY(int row, int col);
